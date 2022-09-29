4c1893727a2b2ee34ce2c32560f122cd9f01903a
Aniket-bansal <akbaisla29041998@gmail.com>
2022-09-29 21:08:51 +0530

    day-3

--- frontend/src/Routes/MainRoutes.jsx
+++ frontend/src/Routes/MainRoutes.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {Routes,Route} from 'react-router-dom'
 import Homepage from '../Pages/Homepage'
+import Pricing from '../Pages/Pricing'
 const MainRoutes = () => {
   return (
     <Routes>
         <Route path="/" element={<Homepage/>} />
-        
+        <Route path="/pricing" element={<Pricing/>}/>
     </Routes>
   )
 }
