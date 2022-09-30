e48c25d253c19746797e606561b620c084ab12cf
Randhir200 <mail.randhirkr@gmail.com>
2022-09-30 17:08:36 +0530

    backend testing completed

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const app = express();
 const connection = require('./config/db');
-const projectRoute = require('./routes/projectRoute')
+const projectRoute = require('./routes/projectRoute');
 
 app.use(express.json());
-app.use('/projects', projectRoute)
-
+app.use('/projects', projectRoute);
 
 const port = process.env.PORT || 7000;
-app.listen(port,async ()=>{
-    try{
-       await connection();
-       console.log('connection established')
-    }catch(e){
-        console.log(e)
-    }
-})
+app.listen(port, async () => {
+  try {
+    await connection();
+    console.log('connection established');
+  } catch (e) {
+    console.log(e);
+  }
+});
