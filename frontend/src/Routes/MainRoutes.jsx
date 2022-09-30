a872816a302e22c0085ed277ecfa8a5c5aefcef6
Aniket-bansal <akbaisla29041998@gmail.com>
2022-09-30 21:39:26 +0530

    conflict update

--- frontend/src/Routes/MainRoutes.jsx
+++ frontend/src/Routes/MainRoutes.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Routes,Route} from 'react-router-dom'
-import Demo from '../Pages/Demo'
+import DemoHome from '../Pages/DemoHome'
 import Homepage from '../Pages/Homepage'
 import Pricing from '../Pages/Pricing'
 const MainRoutes = () => {
@@ -8,7 +8,7 @@ const MainRoutes = () => {
     <Routes>
         <Route path="/" element={<Homepage/>} />
         <Route path="/pricing" element={<Pricing/>}/>
-        <Route path="/demo" element={<Demo/>}/>
+        <Route path="/demo" element={<DemoHome/>}/>
     </Routes>
   )
 }
