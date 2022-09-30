2528a339f490b5f9d4d6da72be6490996a50a8c4
Randhir200 <mail.randhirkr@gmail.com>
2022-09-30 21:47:34 +0530

    demo, price css files deleted and price.jsx file deleted

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const connection = require('./config/db');
-const projectRoute = require('./routes/projectRoute')
-const ClientRouter=require("./routes/clientRouter")
+const projectRoute = require('./routes/projectRoute');
+const ClientRouter = require('./routes/clientRouter');
 app.use(express.json());
-app.use('/projects', projectRoute)
-app.use('/clients', ClientRouter)
+app.use('/projects', projectRoute);
+app.use('/clients', ClientRouter);
 const port = process.env.PORT || 7000;
 app.listen(port, async () => {
   try {
